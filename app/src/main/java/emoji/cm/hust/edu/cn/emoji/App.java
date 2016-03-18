3377a34223ad57d2e065db63d2694dc2ddc40e47
sunmoon <410063005@163.com>
2016-03-18 18:25:12 +0800

    config Timber log

---
+++ app/src/main/java/emoji/cm/hust/edu/cn/emoji/App.java
@@ -0,0 +1,16 @@
+package emoji.cm.hust.edu.cn.emoji;
+
+import android.app.Application;
+
+import timber.log.Timber;
+
+/**
+ * 一些全局配置
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree());
+    }
+}
